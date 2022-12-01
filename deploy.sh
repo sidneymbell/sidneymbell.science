c02be9f468773d0eab5809936108af96ed7f7f3b
Sidney Bell <sidneymbell@gmail.com>
2022-12-01 12:54:13 -0800

    edit deploy.sh to keep cname intact

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ set -e
 git checkout main
 npm run build
 git checkout gh-pages
+mv CNAME ./dist/
 git rm -rf .
 mv dist/* . && rmdir dist/ 
-git add index.html assets/
+git add index.html CNAME assets/
 git commit -m "deploy" --no-verify
 git push
 git checkout main
